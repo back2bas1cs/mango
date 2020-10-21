2fdb306adc435afc4e6c31e989fe4f7fc695b943
JD <james@JDs-MacBook-Pro.local>
2020-10-21 05:19:39 -0700

    First Commit

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// Component(s)
+import App from "./src/App.jsx";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
